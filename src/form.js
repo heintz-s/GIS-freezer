adef8ab279d552d441adb854bc0cc1bd320fc106
heintz-s <stephanie.heintz@web.de>
2022-05-12 00:28:18 +0200

    localStorage

--- src/form.js
+++ src/form.js
@@ -1,11 +1,15 @@
 let saveButton = document.getElementById("save");
 saveButton.addEventListener("click", save);
+let input = document.getElementsByTagName("input");
 function save() {
-  let input = document.getElementsByTagName("input");
-  for(i of input){
-    console.log(i.value);
-  }
-  //window.location = "./index.html"
+  store.addItem({
+    id: id,
+    itemName: input[0].value,
+    imageSrc: input[1].value,
+    expiryDate: new Date(input[2].value),
+    addDate: item? item.addDate : new Date()
+  });
+  window.location = "./index.html"
 }
 
 window.addEventListener("keydown", (event) => 
@@ -14,3 +18,14 @@ window.addEventListener("keydown", (event) =>
     save(event);
   }
 });
+
+const id = new URLSearchParams(window.location.search).get('id');
+const item = store.getItem(id);
+input[2].value = new Date().toLocaleDateString('en-CA');
+if(id){
+  input[0].value = item.itemName;
+  if(item.imageSrc){
+    input[1].value = item.imageSrc;
+  }
+  input[2].value = new Date(item.expiryDate).toLocaleDateString('en-CA');
+}
